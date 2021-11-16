607572954c461fe4234ffec02b9a46df4157f5b4
andrei-filip912 <filip.andrei912@gmail.com>
2021-11-16 11:25:37 +0100

    broken commit
    heatmap does not start

--- src/components/HeatmapGrid.js
+++ src/components/HeatmapGrid.js
@@ -112,7 +112,7 @@ const generateGrid = async (row, col, data) => {
 const HeatmapGrid = ({data}) => {
 	const [grid, setGrid] = useState(null);
 
-	if (!grid) return <div>Loading...</div>;
+	if (!data) return <div>Loading...</div>;
 
 	useEffect(async () => {
 		const grid = await generateGrid(rows, cols, data);
@@ -142,7 +142,7 @@ const HeatmapGrid = ({data}) => {
 		};
 	};
 
-	if (!grid) return <div>Loading...</div>;
+	// if (!grid) return <div>Loading...</div>;
 	return (
 		<div>
 			<AppBar
