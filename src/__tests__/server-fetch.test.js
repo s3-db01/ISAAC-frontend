6c8089ca1c850d702addc0c6ec1fa78acd5d4028
HiteshManglani123 <Hiteshm2002@gmail.com>
2021-11-22 14:17:48 +0100

    tested enum function

--- src/__tests__/server-fetch.test.js
+++ src/__tests__/server-fetch.test.js
@@ -1,113 +1,113 @@
-import {serverFetch} from '../utils/server-fetch';
-import axios from 'axios';
+// import {serverFetch} from '../utils/server-fetch';
+// import axios from 'axios';
 
 
-jest.mock('axios');
+// jest.mock('axios');
 
-// global.axios.get = jest.fn(() =>
-// 	Promise.resolve({
-// 		json: () => Promise.resolve({
-// 			entries:
-//         [
-//         	{
-//         		'id': 1,
-//         		'temp': 25.0,
-//         		'humidity': 45,
-//         		'dateTime': 'October 11, 2021 08:00:00',
-//         	},
-//         	{
-//         		'id': 2,
-//         		'temp': 26.1,
-//         		'humidity': 45,
-//         		'dateTime': 'October 11, 2021 08:10:00',
-//         	},
-//         	{
-//         		'id': 3,
-//         		'temp': 25.9,
-//         		'humidity': 45,
-//         		'dateTime': 'October 11, 2021 08:20:00',
-//         	},
-//         	{
-//         		'id': 4,
-//         		'temp': 26,
-//         		'humidity': 48,
-//         		'dateTime': 'October 11, 2021 08:30:00',
-//         	},
-//         ],
-// 		}),
-// 	}),
-// );
-const entries = [	// mock return value
-	{
-		'id': 1,
-		'temp': 25.0,
-		'humidity': 45,
-		'dateTime': 'October 11, 2021 08:00:00',
-	},
-	{
-		'id': 2,
-		'temp': 26.1,
-		'humidity': 45,
-		'dateTime': 'October 11, 2021 08:10:00',
-	},
-	{
-		'id': 3,
-		'temp': 25.9,
-		'humidity': 45,
-		'dateTime': 'October 11, 2021 08:20:00',
-	},
-	{
-		'id': 4,
-		'temp': 26,
-		'humidity': 48,
-		'dateTime': 'October 11, 2021 08:30:00',
-	},
-];
+// // global.axios.get = jest.fn(() =>
+// // 	Promise.resolve({
+// // 		json: () => Promise.resolve({
+// // 			entries:
+// //         [
+// //         	{
+// //         		'id': 1,
+// //         		'temp': 25.0,
+// //         		'humidity': 45,
+// //         		'dateTime': 'October 11, 2021 08:00:00',
+// //         	},
+// //         	{
+// //         		'id': 2,
+// //         		'temp': 26.1,
+// //         		'humidity': 45,
+// //         		'dateTime': 'October 11, 2021 08:10:00',
+// //         	},
+// //         	{
+// //         		'id': 3,
+// //         		'temp': 25.9,
+// //         		'humidity': 45,
+// //         		'dateTime': 'October 11, 2021 08:20:00',
+// //         	},
+// //         	{
+// //         		'id': 4,
+// //         		'temp': 26,
+// //         		'humidity': 48,
+// //         		'dateTime': 'October 11, 2021 08:30:00',
+// //         	},
+// //         ],
+// // 		}),
+// // 	}),
+// // );
+// const entries = [	// mock return value
+// 	{
+// 		'id': 1,
+// 		'temp': 25.0,
+// 		'humidity': 45,
+// 		'dateTime': 'October 11, 2021 08:00:00',
+// 	},
+// 	{
+// 		'id': 2,
+// 		'temp': 26.1,
+// 		'humidity': 45,
+// 		'dateTime': 'October 11, 2021 08:10:00',
+// 	},
+// 	{
+// 		'id': 3,
+// 		'temp': 25.9,
+// 		'humidity': 45,
+// 		'dateTime': 'October 11, 2021 08:20:00',
+// 	},
+// 	{
+// 		'id': 4,
+// 		'temp': 26,
+// 		'humidity': 48,
+// 		'dateTime': 'October 11, 2021 08:30:00',
+// 	},
+// ];
 
 
-describe('server fetching tests', () => {
-	afterEach(() => {
-		jest.resetModules();
-	});
+// describe('server fetching tests', () => {
+// 	afterEach(() => {
+// 		jest.resetModules();
+// 	});
 
-	it('should return data', async () => {
+// 	it('should return data', async () => {
 		
-		// mocking the function axios.get
-		axios.get = jest.fn().mockImplementationOnce(() =>
-			Promise.resolve({
-				json: () => Promise.resolve({entries})
-			})
-		);
+// 		// mocking the function axios.get
+// 		axios.get = jest.fn().mockImplementationOnce(() =>
+// 			Promise.resolve({
+// 				json: () => Promise.resolve({entries})
+// 			})
+// 		);
 
 
 
-		// calling the function to be tested
-		const rawData = await serverFetch();
+// 		// calling the function to be tested
+// 		const rawData = await serverFetch();
 
-		//expect statements
-		console.log(rawData);
-		// expect(rawData).toEqual(entries);
-		// expect(axios.get).toHaveBeenCalledTimes(1);
-	});
-
-
-	// it('exception handling', async () => {
-	// 	axios.get.mockReturnValueOnce(() =>
-	// 		Promise.reject(Error('Failed to fetch'))
-	// 		// Promise((resolve, reject) => { // eslint-disable-line no-unused-vars
-	// 		// 	setTimeout(() => {
-	// 		// 		reject('Failed to fetch');
-	// 		// 	}, 10);
-	// 		// })
-	// 	);
+// 		//expect statements
+// 		console.log(rawData);
+// 		// expect(rawData).toEqual(entries);
+// 		// expect(axios.get).toHaveBeenCalledTimes(1);
+// 	});
 
 
+// 	// it('exception handling', async () => {
+// 	// 	axios.get.mockReturnValueOnce(() =>
+// 	// 		Promise.reject(Error('Failed to fetch'))
+// 	// 		// Promise((resolve, reject) => { // eslint-disable-line no-unused-vars
+// 	// 		// 	setTimeout(() => {
+// 	// 		// 		reject('Failed to fetch');
+// 	// 		// 	}, 10);
+// 	// 		// })
+// 	// 	);
 
-	// 	const rawData = await serverFetch();
-	// 	console.log(rawData.message);
 
 
-	// 	// expect(rawData.message).toEqual('Failed to fetch');
-	// 	// expect(fetch).toHaveBeenCalledTimes(1);
-	// });
-});
+// 	// 	const rawData = await serverFetch();
+// 	// 	console.log(rawData.message);
+
+
+// 	// 	// expect(rawData.message).toEqual('Failed to fetch');
+// 	// 	// expect(fetch).toHaveBeenCalledTimes(1);
+// 	// });
+// });
