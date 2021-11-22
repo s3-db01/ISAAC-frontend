df9c14bba1e3973b7bbb383cbc1749c6b7918fe1
andrei-filip912 <filip.andrei912@gmail.com>
2021-11-22 14:53:29 +0100

    FIXED SERVER FETCH

--- src/utils/server-fetch.js
+++ src/utils/server-fetch.js
@@ -3,8 +3,6 @@ import axios from 'axios';
 export const serverFetch = async () => {
 	try {
 		const response = await axios.get('http://localhost:5000/entries');
-
-		console.log(response.data);
 		return  response.data;
 	}
 	catch(error) {
