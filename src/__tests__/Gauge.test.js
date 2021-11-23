3954dd7af24d265c266f3028aa0aff312534fb34
HiteshManglani123 <Hiteshm2002@gmail.com>
2021-11-23 10:02:26 +0100

    Update Gauge.test.js

--- src/__tests__/Gauge.test.js
+++ src/__tests__/Gauge.test.js
@@ -2,16 +2,52 @@ import React from 'react';
 import Gauge from '../components/Gauge';
 import { shallow, render, mount, configure } from 'enzyme';
 import toJSON from 'enzyme-to-json';
+import {act} from 'react-dom/test-utils';
+const data = [
+	{
+		'id': 1,
+		'temp': 25.0,
+		'humidity': 45,
+		'dateTime': 'October 11, 2021 08:00:00',
+	},
+	{
+		'id': 2,
+		'temp': 26.1,
+		'humidity': 45,
+		'dateTime': 'October 11, 2021 08:10:00',
+	},
+	{
+		'id': 3,
+		'temp': 25.9,
+		'humidity': 45,
+		'dateTime': 'October 11, 2021 08:20:00',
+	},
+	{
+		'id': 4,
+		'temp': 26,
+		'humidity': 48,
+		'dateTime': 'October 11, 2021 08:30:00',
+	},
+];
 
-const data = {
-
-};
-
-test('should render NavBar without crashing', () => {
-	shallow(<Gauge />);
+test('adds state to the component', () => {
+	let myComponent;
+	let instance;
+	act(() => {
+		myComponent = mount(<Gauge name='Temperature' data={data} />);
+		myComponent.setState({...data});
+		instance = myComponent.instance();
+	});
+	expect(instance.state).toEqual(data);
 });
 
-test('should render NavBar correctly', () => {
-	const tree = shallow(<Gauge name='Temperature' data={data}/>);
-	expect(toJSON(tree)).toMatchSnapshot();
-});
+// test('should render Gauge correctly', () => {
+// 	const realUseState = React.useState;
+
+// 	const stubInitialState = data;
+// 	jest
+// 		.spyOn(React, 'useState')
+// 		.mockImplementationOnce(() => realUseState(stubInitialState));
+// 	const tree = mount(<Gauge name='Temperature' data={data}/>);
+// 	expect(toJSON(tree)).toMatchSnapshot();
+// });
