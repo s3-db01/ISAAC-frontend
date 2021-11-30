3c0e04c57a12e02cb3c818f824d1413d067e269d
HiteshManglani123 <Hiteshm2002@gmail.com>
2021-11-30 14:12:33 +0100

    Update server-fetch.js

--- src/utils/server-fetch.js
+++ src/utils/server-fetch.js
@@ -4,413 +4,148 @@
 export const serverFetch = async () => {
 	return [
 		{
-			'id': 0,
-			'x': 5,
-			'y': 3,
-			'temp': 23.6,
-			'humidity': 45,
-			'dateTime' : 'November 08, 2021 08:00:00'
-			  
+		  'id': 1,
+		  'x': 4,
+		  'y': 3,
+		  'temp': 23.6,
+		  'humidity': 45,
+		  'dateTime' : 'November 29, 2021 08:00:00'
+		  
 		},
 		{
-			  'id': 1,
-			  'x': 4,
-			  'y': 3,
-			  'temp': 22,
-			  'humidity': 45,
-			  'dateTime' : 'November 01, 2021 08:00:00'
+		  'id': 2,
+		  'x': 3,
+		  'y': 8,
+		  'temp': 22,
+		  'humidity': 47,
+		  'dateTime' : 'November 29, 2021 08:00:00'
 		},
 		{
-			  'id': 1,
-			  'x': 4,
-			  'y': 3,
-			  'temp': 24,
-			  'humidity': 45,
-			  'dateTime' : 'November 01, 2021 12:00:00'
+		  'id': 3,
+		  'x': 3,
+		  'y': 12,
+		  'temp': 24,
+		  'humidity': 50,
+		  'dateTime' : 'November 29, 2021 08:00:00'
 		},
 		{
-			  'id': 1,
-			  'x': 4,
-			  'y': 3,
-			  'temp': 24,
-			  'humidity': 40,
-			  'dateTime' : 'November 01, 2021 17:00:00'
+		  'id': 4,
+		  'x': 11,
+		  'y': 4,
+		  'temp': 25,
+		  'humidity': 40,
+		  'dateTime' : 'November 29, 2021 08:00:00'
 		},
 		{
-			  'id': 2,
-			  'x': 3,
-			  'y': 8,
-			  'temp': 20,
-			  'humidity': 45,
-			  'dateTime' : 'November 01, 2021 08:00:00'
+		  'id': 5,
+		  'x': 11,
+		  'y': 6,
+		  'temp': 25.3,
+		  'humidity': 45,
+		  'dateTime' : 'November 29, 2021 08:00:00'
+		},
+		
+	  
+	  
+		{
+		  'id': 6,
+		  'x': 4,
+		  'y': 3,
+		  'temp': 23.7,
+		  'humidity': 43,
+		  'dateTime' : 'November 29, 2021 08:05:00'
+		  
 		},
 		{
-			  'id': 2,
-			  'x': 3,
-			  'y': 8,
-			  'temp': 22,
-			  'humidity': 45,
-			  'dateTime' : 'November 02, 2021 12:00:00'
+		  'id': 7,
+		  'x': 3,
+		  'y': 8,
+		  'temp': 22.5,
+		  'humidity': 45,
+		  'dateTime' : 'November 29, 2021 08:05:00'
 		},
 		{
-			  'id': 2,
-			  'x': 3,
-			  'y': 8,
-			  'temp':  21,
-			  'humidity': 40,
-			  'dateTime' : 'November 02, 2021 17:00:00'
+		  'id': 8,
+		  'x': 3,
+		  'y': 12,
+		  'temp': 24.4,
+		  'humidity': 47,
+		  'dateTime' : 'November 29, 2021 08:05:00'
 		},
 		{
-			  'id': 13213,
-			  'x': 3,
-			  'y': 9,
-			  'temp':  22,
-			  'humidity': 40,
-			  'dateTime' : 'November 02, 2021 17:00:00'
+		  'id': 9,
+		  'x': 11,
+		  'y': 4,
+		  'temp': 27,
+		  'humidity': 41,
+		  'dateTime' : 'November 29, 2021 08:05:00'
 		},
 		{
-			  'id': 3,
-			  'x': 3,
-			  'y': 12,
-			  'temp': 26,
-			  'humidity': 45,
-			  'dateTime' : 'November 03, 2021 08:00:00'
+		  'id': 10,
+		  'x': 11,
+		  'y': 6,
+		  'temp': 25.3,
+		  'humidity': 45,
+		  'dateTime' : 'November 29, 2021 08:05:00'
 		},
 		{
-			  'id': 3,
-			  'x': 3,
-			  'y': 12,
-			  'temp': 24.5,
-			  'humidity': 45,
-			  'dateTime' : 'November 03, 2021 12:00:00'
+		  'id': 10,
+		  'x': 11,
+		  'y': 12,
+		  'temp': 25.3,
+		  'humidity': 45,
+		  'dateTime' : 'November 29, 2021 08:09:00'
+		},
+	  
+	  
+		{
+		  'id': 11,
+		  'x': 4,
+		  'y': 3,
+		  'temp': 23.4,
+		  'humidity': 45,
+		  'dateTime' : 'November 29, 2021 08:10:00'
+		  
 		},
 		{
-			  'id': 3,
-			  'x': 4,
-			  'y': 12,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 03, 2021 17:00:00'
+		  'id': 12,
+		  'x': 3,
+		  'y': 8,
+		  'temp': 22.2,
+		  'humidity': 47,
+		  'dateTime' : 'November 29, 2021 08:10:00'
 		},
 		{
-			  'id': 4,
-			  'x': 11,
-			  'y': 4,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 04, 2021 17:00:00'
+		  'id': 13,
+		  'x': 3,
+		  'y': 12,
+		  'temp': 23,
+		  'humidity': 50,
+		  'dateTime' : 'November 29, 2021 08:10:00'
 		},
 		{
-			  'id': 4,
-			  'x': 11,
-			  'y': 4,
-			  'temp': 20,
-			  'humidity': 40,
-			  'dateTime' : 'November 04, 2021 17:00:00'
+		  'id': 14,
+		  'x': 11,
+		  'y': 4,
+		  'temp': 24.8,
+		  'humidity': 42,
+		  'dateTime' : 'November 29, 2021 08:10:00'
 		},
 		{
-			  'id': 4,
-			  'x': 10,
-			  'y': 4,
-			  'temp': 10,
-			  'humidity': 40,
-			  'dateTime' : 'November 04, 2021 17:00:00'
-		},
-		{
-			  'id': 4,
-			  'x': 11,
-			  'y': 4,
-			  'temp': 21,
-			  'humidity': 40,
-			  'dateTime' : 'November 04, 2021 17:00:00'
-		},
-		{
-			  'id': 5,
-			  'x': 11,
-			  'y': 6,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 05, 2021 17:00:00'
-		},
-		{
-			  'id': 5,
-			  'x': 11,
-			  'y': 6,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 05, 2021 17:00:00'
-		},
-		{
-			  'id': 5,
-			  'x': 11,
-			  'y': 6,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 05, 2021 17:00:00'
-		},
-		{
-			  'id': 6,
-			  'x': 11,
-			  'y': 12,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 06, 2021 17:00:00'
-		},
-		{
-			  'id': 6,
-			  'x': 11,
-			  'y': 12,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 06, 2021 17:00:00'
-		},
-		{
-			  'id': 6,
-			  'x': 11,
-			  'y': 12,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 06, 2021 17:00:00'
-		},
-		{
-			  'id': 7,
-			  'x': 18,
-			  'y': 13,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 07, 2021 17:00:00'
-		},
-		{
-			  'id': 7,
-			  'x': 18,
-			  'y': 13,
-			  'temp': 28,
-			  'humidity': 40,
-			  'dateTime' : 'November 07, 2021 17:00:00'
-		},
-		{
-			  'id': 7,
-			  'x': 18,
-			  'y': 13,
-			  'temp': 28,
-			  'humidity': 40,
-			  'dateTime' : 'November 07, 2021 17:00:00'
-		},
-		{
-			  'id': 8,
-			  'x': 22,
-			  'y': 3,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 08, 2021 17:00:00'
-		},
-		{
-			  'id': 8,
-			  'x': 22,
-			  'y': 3,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 08, 2021 17:00:00'
-		},
-		{
-			  'id': 8,
-			  'x': 22,
-			  'y': 3,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 08, 2021 17:00:00'
-		},
-		{
-			  'id': 9,
-			  'x': 22,
-			  'y': 13,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 09, 2021 17:00:00'
-		},
-		{
-			  'id': 9,
-			  'x': 22,
-			  'y': 13,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 09, 2021 17:00:00'
-		},
-		{
-			  'id': 9,
-			  'x': 22,
-			  'y': 13,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 09, 2021 17:00:00'
-		},
-		{
-			  'id': 10,
-			  'x': 25,
-			  'y': 3,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 10, 2021 17:00:00'
-		},
-		{
-			  'id': 10,
-			  'x': 25,
-			  'y': 3,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 10, 2021 17:00:00'
-		},
-		{
-			  'id': 10,
-			  'x': 25,
-			  'y': 3,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 10, 2021 17:00:00'
-		},
-		{
-			  'id': 11,
-			  'x': 24,
-			  'y': 5,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 11, 2021 17:00:00'
-		},
-		{
-			  'id': 11,
-			  'x': 24,
-			  'y': 5,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 11, 2021 17:00:00'
-		},
-		{
-			  'id': 11,
-			  'x': 24,
-			  'y': 5,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 11, 2021 17:00:00'
-		},
-		{
-			  'id': 12,
-			  'x': 24,
-			  'y': 8,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 12, 2021 17:00:00'
-		},
-		{
-			  'id': 12,
-			  'x': 24,
-			  'y': 8,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 12, 2021 17:00:00'
-		},
-		{
-			  'id': 12,
-			  'x': 24,
-			  'y': 8,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 12, 2021 17:00:00'
-		},
-		{
-			  'id': 13,
-			  'x': 25,
-			  'y': 13,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 13, 2021 17:00:00'
-		},
-		{
-			  'id': 13,
-			  'x': 25,
-			  'y': 13,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 13, 2021 17:00:00'
-		},
-		{
-			  'id': 13,
-			  'x': 25,
-			  'y': 13,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 13, 2021 17:00:00'
-		},
-		{
-			  'id': 14,
-			  'x': 30,
-			  'y': 1,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 14, 2021 17:00:00'
-		},
-		{
-			  'id': 14,
-			  'x': 30,
-			  'y': 1,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 14, 2021 17:00:00'
-		},
-		{
-			  'id': 14,
-			  'x': 30,
-			  'y': 1,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 14, 2021 17:00:00'
-		},
-		{
-			  'id': 15,
-			  'x': 30,
-			  'y': 7,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November  15, 2021 17:00:00'
-		},
-		{
-			  'id': 15,
-			  'x': 30,
-			  'y': 7,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November  15, 2021 17:00:00'
-		},
-		{
-			  'id': 15,
-			  'x': 30,
-			  'y': 7,
-			  'temp': 23,
-			  'humidity': 40,
-			  'dateTime' : 'November 15, 2021 17:00:00'
-		},
-		{
-			  'id': 16,
-			  'x': 31,
-			  'y': 13,
-			  'temp': 28,
-			  'humidity': 40,
-			  'dateTime' : 'November 16, 2021 17:00:00'
-		},
-		{
-			  'id': 16,
-			  'x': 31,
-			  'y': 13,
-			  'temp': 28,
-			  'humidity': 40,
-			  'dateTime' : 'November 16, 2021 17:00:00'
-		},
-		{
-			  'id': 16,
-			  'x': 31,
-			  'y': 13,
-			  'temp': 28,
-			  'humidity': 40,
-			  'dateTime' : 'November 16, 2021 17:00:00'
-		}
-		  ];
+			'id': 14,
+			'x': 11,
+			'y': 4,
+			'temp': 24.8,
+			'humidity': 42,
+			'dateTime' : 'November 30, 2021 08:10:00'
+		  },
+		  {
+			'id': 14,
+			'x': 11,
+			'y': 4,
+			'temp': 24.8,
+			'humidity': 42,
+			'dateTime' : 'November 30, 2021 08:10:00'
+		  },
+	];
 };
